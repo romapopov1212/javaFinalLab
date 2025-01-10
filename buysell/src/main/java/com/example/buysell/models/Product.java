e150faf29e002149d041c2f6bed8f8351388ae6a
Popov Roman <popovrromma@gmail.com>
2025-01-10 17:47:24 +1000

    init

---
+++ buysell/src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,9 @@
+package com.example.buysell.models;
+
+public class Product {
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}

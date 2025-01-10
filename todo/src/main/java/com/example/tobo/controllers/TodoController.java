2e0cc60a15bccdd987d8b1c4843eb441c343f2ec
Popov Roman <popovrromma@gmail.com>
2025-01-10 20:59:57 +1000

    add postgres database and base functional to interaction with task(add, delete, search)

--- todo/src/main/java/com/example/tobo/controllers/TodoController.java
+++ todo/src/main/java/com/example/tobo/controllers/TodoController.java
@@ -5,9 +5,12 @@ import com.example.tobo.repositories.TodoItemRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Controller
 public class TodoController implements CommandLineRunner {
@@ -22,12 +25,47 @@ public class TodoController implements CommandLineRunner {
     public String index(Model model){
         List<TodoItem> allTodos =  todoItemRepository.findAll();
         model.addAttribute("allTodos", allTodos);
+        model.addAttribute("newTodo", new TodoItem());
+        return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute TodoItem todoItem){
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") int id){
+        todoItemRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteAll")
+    public String deleteAll(){
+        todoItemRepository.deleteAll();
+        return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam("searchTerm") String searchTerm, Model model){
+        List<TodoItem> allItems = todoItemRepository.findAll();
+        List<TodoItem> searchResults = new ArrayList<>();
+
+        for (TodoItem item : allItems){
+            if (item.getTitle().toLowerCase().contains(searchTerm.toLowerCase())){
+                searchResults.add(item);
+            }
+        }
+        model.addAttribute("allTodos", searchResults);
+        model.addAttribute("newTodo", new TodoItem());
+        model.addAttribute("searchTerm", searchTerm);
         return "index";
     }
 
     @Override
     public void run(String... args) throws Exception {
-        todoItemRepository.save(new TodoItem("Item 1"));
-        todoItemRepository.save(new TodoItem("Item 2"));
+
     }
 }
